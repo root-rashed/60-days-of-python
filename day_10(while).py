dc3f2e178aadefa62554611479be92a6514e559b
rashed <rashedulislam.shajol@gmail.com>
2026-03-17 11:55:10 +0600

    update

--- day_10(while).py
+++ day_10(while).py
@@ -11,3 +11,10 @@ while index<len(data):
     print(data[index])
     index+=1
 
+nums =[1,2,3,4,5,6,7,8,9,10]
+sum = 0
+index =0
+while index<len(nums):
+    sum+=nums[index]
+    print(index,nums[index],sum)
+    index+=1
