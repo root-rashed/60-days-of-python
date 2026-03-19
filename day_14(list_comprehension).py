d0648703d3a14fc9afa75eff7c18ccd4b95174a5
rashed <rashedulislam.shajol@gmail.com>
2026-03-19 13:34:48 +0600

    14

---
+++ day_14(list_comprehension).py
@@ -0,0 +1,17 @@
+squares = [x**2 for x in range(10)]
+print(squares)
+
+
+# Condition
+evens = [x for x in range(10) if x%2 == 0]
+print(evens)
+
+
+
+matrix = [[j for j in range(5)] for i in range(3)]
+print(matrix)
+
+
+words = ['python','list','ai','data','science']
+lengths = [len(word) for word in words]
+print(lengths)
