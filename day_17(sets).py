f0245ae3bda162d1256b5b0dc07fe9945086de72
rashed <rashedulislam.shajol@gmail.com>
2026-03-20 15:55:26 +0600

    17

--- day_17(sets).py
+++ day_17(sets).py
@@ -8,8 +8,10 @@ set1 ={}
 print(type(set1))
 
 
+
 # set3 = set()
 # print(type(set3))
+
 
 
 # Set in unordered
@@ -17,6 +19,12 @@ set.add(100)
 print(set)
 
 
+
 # Unique value
 set.update([1000,1000,1000])
+print(set)
+
+
+
+set.remove(1000)
 print(set)
