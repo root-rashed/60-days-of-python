8af849d485968587a477147be4b1b734865f8bf3
rashed <rashedulislam.shajol@gmail.com>
2026-03-20 13:50:38 +0600

    16

---
+++ day_15(tuple).py
@@ -0,0 +1,3 @@
+# After assign can't be changed
+tuple = (1,100,True,'study',[1,2,3])
+print(tuple)
---
+++ day_16(more_tuple).py
@@ -0,0 +1,10 @@
+# After assign can't be changed
+tuple = (1,100,True,'study',[1,2,3])
+print(tuple)
+print(1 in tuple)
+print(type(tuple))
+
+print(tuple.count(3))
+
+tuple*3
+print(tuple)
