4bf6a64fdd6bbf82e3d3aa0931d49657e174c5b6
rashed <rashedulislam.shajol@gmail.com>
2026-03-26 16:31:49 +0600

    dic

---
+++ day_21(all_about_dics).py
@@ -0,0 +1,19 @@
+dic ={
+    'name':"rashed",
+    'varsity':"SEU"
+}
+print(dic)
+print(dic.keys())
+print(dic.values())
+
+
+
+dic2 = dic.copy()
+print(dic2)
+
+dic['varsity'] = 'TUM'
+print(dic)
+
+
+dic['dep'] = 'CSE'
+print(dic)
