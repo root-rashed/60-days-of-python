59dd2c92842c65f76a9d0124ec87eacbf0334cdf
rashed <rashedulislam.shajol@gmail.com>
2026-03-27 12:51:21 +0600

    built in

---
+++ day_28(built_in).py
@@ -0,0 +1,52 @@
+print("rashed")
+
+
+ls = [1,2,3,4,5]
+length = len(ls)
+print(length)
+
+
+user_input = int(input("Enter any number: "))
+print(user_input)
+
+
+print(type(user_input))
+
+
+flt = float(user_input)
+print(type(flt))
+
+
+numbers = list(range(1,10))
+print(numbers)
+
+print(min(numbers))
+print(max(numbers))
+
+
+absolute = abs(-10)
+print(absolute)
+
+
+
+rounded = round(3.1415,3)
+print(rounded)
+
+
+sorted_numbers = sorted(numbers)
+print(numbers)
+
+
+
+
+slice_list = slice(1,4)
+print(slice_list)
+
+
+
+#  chr() and ord() and ASCII
+euro_symbol = chr(8364)
+code_point = ord('€')
+print(euro_symbol)
+print(code_point)
+
