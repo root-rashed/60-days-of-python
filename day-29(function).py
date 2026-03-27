520d7b0f411eb481fcf3934e09ff6282b52d7862
rashed <rashedulislam.shajol@gmail.com>
2026-03-27 22:42:23 +0600

    function

---
+++ day-29(function).py
@@ -0,0 +1,14 @@
+def dept():
+    print("Hello python")
+dept()
+
+
+def dep(name):
+    print("Your name is {}".format(name))
+dep("Rashed")
+
+
+# Function
+def add(num1,num2):
+    return num1+num2
+print(add(1,2))
