1f73ad360af48a3313b3f957d964732d603c51e1
rashed <rashedulislam.shajol@gmail.com>
2026-03-27 11:09:52 +0600

    break

---
+++ day_26(break_continue).py
@@ -0,0 +1,13 @@
+for i in range(100):
+    if i==25:
+        break
+    else:
+        print(i)
+
+
+# 3 times
+for i in range(1,11):
+    if i==11:
+        break
+    else:
+        print("3*{} = {}".format(i,i*3))
