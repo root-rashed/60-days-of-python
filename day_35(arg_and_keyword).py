3a262e35b4a753e8b9fb14a35d9a12be2afdfffd
rashed <rashedulislam.shajol@gmail.com>
2026-03-28 14:48:18 +0600

    35

--- day_35(arg_and_keyword).py
+++ day_35(arg_and_keyword).py
@@ -1,11 +1,14 @@
 def personal_data(name,**arg):
     print(name)
     print(arg)
+    print(type(arg))
 personal_data("Rashed",varsity="SEU",city="Dhaka")
+
 
 
 
 def personal_data(name,*arg):
     print(name)
     print(arg)
+    print(type(arg))
 personal_data("Rashed","SEU","Dhaka")
