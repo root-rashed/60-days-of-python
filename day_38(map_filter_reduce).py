4865a62538511fa864e2e73d7ea8a1c5a75741fc
rashed <rashedulislam.shajol@gmail.com>
2026-03-28 15:18:57 +0600

    38

--- day_38(map_filter_reduce).py
+++ day_38(map_filter_reduce).py
@@ -4,3 +4,12 @@ print(num)
 
 sqr = list(map(lambda x:x**2,num))
 print(sqr)
+
+
+fil = list(filter(lambda num: num%2==1,num))
+print(fil)
+
+
+from functools import reduce
+after_reduced = reduce(lambda a,b:a+b,num)
+print(after_reduced)
