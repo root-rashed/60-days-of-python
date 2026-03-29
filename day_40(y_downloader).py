3a17658adebf3709d763568bfa3a2b0fcb51edf5
rashed <rashedulislam.shajol@gmail.com>
2026-03-29 09:55:27 +0600

    yt_dlp

--- day_40(y_downloader).py
+++ day_40(y_downloader).py
@@ -1,11 +1,16 @@
 import yt_dlp
 
-url = "https://youtu.be/6MgsHSAcI9k?si=gSm13TkPUJ76hZmW"
+url = "https://youtu.be/6MgsHSAcI9k"
 
-# Download lowest resolution (like your original code)
 ydl_opts = {
-    'format': 'worstvideo+worstaudio/worst',
-    'outtmpl': '%(title)s.%(ext)s',  # saves as video title
+    'format': 'worst',
+    'outtmpl': '%(title)s.%(ext)s',
+    'cookiefile': r'C:\Users\anonymous\Desktop\cookies.txt',
+    'extractor_args': {
+        'youtube': {
+            'player_client': ['web'],  # uses JS runtime (Node.js)
+        }
+    },
 }
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
