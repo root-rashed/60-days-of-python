6cad3f1a421fec6cf799bea6697f69a7ae2a3104
rashed <rashedulislam.shajol@gmail.com>
2026-03-29 13:52:50 +0600

    generator

---
+++ day_43(generator).py
@@ -0,0 +1,15 @@
+# yield statement is uses to return one value at a time
+def count_up_to(max):
+    count =1
+    while count <=max:
+        yield count
+        count+=1
+
+counter = count_up_to(5)
+
+
+print(next(counter))
+print(next(counter))
+print(next(counter))
+print(next(counter))
+print(next(counter))
